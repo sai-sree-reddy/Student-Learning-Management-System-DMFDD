b02d4bc7af85f0ee49e0cd16d179a9e35f84d7a0
sai-sree-reddy <114794111+sai-sree-reddy@users.noreply.github.com>
2022-11-30 22:25:42 -0500

    TRIGGERS

---
+++ DML TRIGGERS.sql
@@ -0,0 +1,15 @@
+SELECT * FROM Category
+GO
+GO
+CREATE TRIGGER CHANGECATEGORYNAME1 ON Category
+FOR INSERT
+AS
+BEGIN 
+    UPDATE Category
+    set CategoryDescription = 'Artificial Intelligence Methods' where CategoryID = 'AI'
+	UPDATE Category
+    set CategoryDescription = 'BigData Analytics Tools' where CategoryID = 'BDA'
+END
+GO
+
+INSERT INTO Category Values ('HSL', 'Human Services Tools')
