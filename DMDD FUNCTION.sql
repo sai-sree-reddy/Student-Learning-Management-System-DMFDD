4f77d6febb32acf43bc6b9296d7a849bc6ded717
sai-sree-reddy <114794111+sai-sree-reddy@users.noreply.github.com>
2022-11-30 22:27:46 -0500

    FUNCTIONS

---
+++ DMDD FUNCTION.sql
@@ -0,0 +1,18 @@
+Create Function CalcAge (@dob date)
+ Returns Int
+ As
+ Begin
+ declare @age int
+ set @dob='2000-08-18'
+ set @age = DATEDIFF(YEAR,@dob,GETDATE()) -
+				Case
+					When (Month(@dob)> MONTH(GETDATE()) ) or 
+						 (MONTH(@dob)=MONTH(GETDATE()) and DAY (@dob)> DAY(GETDATE()))
+					Then 1
+					else 0
+					End
+Return @age
+ End
+ GO
+
+ Select dbo.CalcAge('2000-09-18') as Age_of_Student
